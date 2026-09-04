71ccc783139605e57e56ad6be7d67b521a4c5fa8
Shine0111 <ranshine9@gmail.com>
2026-09-04 00:46:07 -0400

    refactor for error handle

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -4,7 +4,11 @@ import User from "../models/User";
 import generateToken from "../utils/generateToken";
 import { AuthRequest } from "../middleware/authMiddleware";
 import { asyncHandler } from "../utils/asyncHandler";
-import { BadRequestError, ConflictError } from "../utils/errors";
+import {
+  BadRequestError,
+  ConflictError,
+  UnauthorizedError,
+} from "../utils/errors";
 
 export const registerUser = asyncHandler(
   async (req: Request, res: Response) => {
@@ -43,43 +47,39 @@ export const registerUser = asyncHandler(
   },
 );
 
-export const loginUser = async (req: Request, res: Response) => {
-  try {
-    const { email, password } = req.body;
+export const loginUser = asyncHandler(async (req: Request, res: Response) => {
+  const { email, password } = req.body;
 
-    // Check if info is not complete
-    if (!email || !password) {
-      res.status(400).json({ message: "Email and password are required" });
-    }
-
-    // Find user by email
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-
-    // Check if password matches
-    const isMatch = await bcrypt.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-
-    // Generate token
-    const token = generateToken(user._id.toString());
-
-    res.status(200).json({
-      id: user._id,
-      name: user.name,
-      email: user.email,
-      token,
-    });
-  } catch (error) {
-    res.status(500).json({ message: "Server error", error });
+  // Check if info is not complete
+  if (!email || !password) {
+    throw new BadRequestError("Email and password are required");
   }
-};
 
-export const getMe = async (req: AuthRequest, res: Response) => {
+  // Find user by email
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new UnauthorizedError("Invalid credentials");
+  }
+
+  // Check if password matches
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new UnauthorizedError("Invalid credentials");
+  }
+
+  // Generate token
+  const token = generateToken(user._id.toString());
+
+  res.status(200).json({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    token,
+  });
+});
+
+export const getMe = asyncHandler(async (req: AuthRequest, res: Response) => {
   res.status(200).json({
     user: req.user,
   });
-};
+});
